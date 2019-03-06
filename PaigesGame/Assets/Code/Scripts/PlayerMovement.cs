3321c296eafd4b4e523ab0e98798ba80d5c8c256
Matt Warneke <warneke.matt@gmail.com>
2019-03-05 22:46:58 -0800

    Mouse movement Scene setup - bedroom start

--- PaigesGame/Assets/Code/Scripts/PlayerMovement.cs
+++ PaigesGame/Assets/Code/Scripts/PlayerMovement.cs
@@ -1,8 +1,9 @@
-﻿using UnityEngine;
-using Assets.Code;
-using Assets.Code.Enums;
-using System.Collections;
+﻿using Assets.Code.Enums;
 using Assets.Code.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Assets.Code
 {
@@ -33,6 +34,11 @@ namespace Assets.Code
         /// Is the player dead?
         /// </summary>
         private bool isDead = false;
+
+        private List<SpriteRenderer> AllChildSprites;
+        //public GameObject CharacterBody;
+
+        bool isFacingLeft = true;
         #endregion
 
         // Use this for initialization
@@ -44,6 +50,8 @@ namespace Assets.Code
             // set initial position
             lastPosition = transform.position;
             CheckPointPosition = transform.position;
+
+            AllChildSprites = this.GetComponentsInChildren<SpriteRenderer>().ToList();
         }
 
         /// <summary>
@@ -60,18 +68,78 @@ namespace Assets.Code
         {
         }
 
+        private Vector3? GetInputPosition()
+        {
+            if (Input.touchSupported
+                && Input.touchCount > 0
+                && Application.platform != RuntimePlatform.WebGLPlayer)
+            {
+                Touch touch = Input.GetTouch(0);
+                //touch.position
+                return new Vector3(0, 0, 0);
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            return null;
+        }
+
+
+        private List<MoveInputDirection> getDirectionToInput(Vector3 inputPosition)
+        {
+            List<MoveInputDirection> inputDirections = new List<MoveInputDirection>();
+            // is input a tiny bit away from character - stops jittering.
+            if (Math.Abs(inputPosition.x - this.transform.position.x) > 0.25f)
+            {   // x increases to the right
+                if (inputPosition.x < this.transform.position.x)
+                    inputDirections.Add(MoveInputDirection.WalkLeft);
+                else if (inputPosition.x > this.transform.position.x)
+                    inputDirections.Add(MoveInputDirection.WalkRight);
+            }
+            if (inputDirections.Count == 0)
+                inputDirections.Add(MoveInputDirection.NoMovement);
+
+            if (Math.Abs(inputPosition.y - this.transform.position.y) > 0.25f)
+            {
+                if (inputPosition.y > this.transform.position.y)
+                    inputDirections.Add(MoveInputDirection.WalkUp);
+                else if (inputPosition.y < this.transform.position.y)
+                    inputDirections.Add(MoveInputDirection.WalkDown);
+            }
+
+            if (inputDirections.Count <= 1)
+                inputDirections.Add(MoveInputDirection.NoMovement);
+
+            return inputDirections;
+        }
+
         // Update is called once per frame
         void Update()
         {
-            MoveInputDirection directionX = InputCalculator.MovementInputX(Input.GetAxis("Horizontal"));
-            MoveInputDirection directionY = InputCalculator.MovementInputY(Input.GetAxis("Vertical"));
+            MoveInputDirection directionX;
+            MoveInputDirection directionY;
+            Vector3? inputPosition = GetInputPosition();
+            if (inputPosition.HasValue)
+            {
+                List<MoveInputDirection> inputDirections = getDirectionToInput(inputPosition.Value);
+                directionX = inputDirections[0];
+                directionY = inputDirections[1];
+
+            }
+            else
+            {
+                directionX = InputCalculator.MovementInputX(Input.GetAxis("Horizontal"));
+                directionY = InputCalculator.MovementInputY(Input.GetAxis("Vertical"));
+            }
 
             float xMovement = (speed.x * (float)directionX) * 5f;
             float yMovement = (speed.y * (float)directionY) * 5f;
 
             // 4 - Movement per direction
             movement = new Vector2(xMovement, yMovement);
-            
+                        
             //if (directionX == MoveInputDirection.WalkRight)
             //{
                 //animator.SetInteger("Direction", (int)MoveAnimDirection.WalkRight);
@@ -95,13 +163,26 @@ namespace Assets.Code
             //}
 
             body.velocity = movement;
-            
-            //if (directionX == MoveInputDirection.NoMovement
-            //    && directionY  == MoveInputDirection.NoMovement)
-            //{
-            //    // we aren't moving so make sure we dont animate
-            //    animator.speed = 0.0f;
-            //}
+
+            if (directionX == MoveInputDirection.WalkRight && isFacingLeft)
+            {
+                FlipRight();
+            }
+            else if (directionX == MoveInputDirection.WalkLeft && !isFacingLeft)
+            {
+                FlipLeft();
+            }
+
+            if (directionX == MoveInputDirection.NoMovement
+                && directionY == MoveInputDirection.NoMovement)
+            {
+                // we aren't moving so make sure we dont animate
+                animator.speed = 0.0f;
+            }
+            else
+            {
+                animator.speed = 2f;
+            }
 
             // if we are dead do not move anymore
             if (isDead == true)
@@ -109,6 +190,34 @@ namespace Assets.Code
                 GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
                 animator.speed = 0.0f;
             }
+        }
+
+        public void FlipRight()
+        {
+            isFacingLeft = false;
+            //CharacterBody.transform.rotation.z = 180;
+            this.transform.Rotate(0, 180, 0);
+            //foreach (SpriteRenderer sprite in AllChildSprites)
+            //    sprite.flipY = true;
+            //float newX = CharacterBody.transform.localPosition.x * -1;
+            //CharacterBody.transform.localPosition =
+            //    new Vector3(newX,
+            //        CharacterBody.transform.localPosition.y,
+            //        CharacterBody.transform.localPosition.z);
+            //Vector3 eulerAngles = CharacterBody.transform.localEulerAngles;
+            //// rotate on y
+            //CharacterBody.transform.localEulerAngles =
+            //    new Vector3(eulerAngles.x,
+            //        (eulerAngles.y == 180 ? 0 : 180),
+            //        eulerAngles.z);
+        }
+
+        public void FlipLeft()
+        {
+            isFacingLeft = true;
+            this.transform.Rotate(0, 180, 0);
+            //foreach (SpriteRenderer sprite in AllChildSprites)
+            //    sprite.flipY = false;
         }
 
         void FixedUpdate()
--- PaigesGame/Assets/Code/Scripts/Tile.cs
+++ PaigesGame/Assets/Code/Scripts/Tile.cs
@@ -21,27 +21,28 @@ public class Tile : MonoBehaviour
 		// obtain the local references
 		playerTransform = GameObject.Find("PlayerCharacter").transform;
 		spriteRenderer = this.GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+            spriteRenderer = this.GetComponentInChildren<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-
+        if (spriteRenderer == null || this.tag != "LargeTile")
+            return;
 		// to ensure correct positioning of the environment around the player (3D Depth Effect)
 		// we need to make the tiles below the player higher than the player in the render layering
 		// and the ones above the player be lower than the player in the render layering
-		if(this.tag == "LargeTile")
+
+		if((playerTransform.position.y - 1.28f) > this.transform.position.y)
 		{
-			if((playerTransform.position.y - 1.28f) > this.transform.position.y)
-			{
-				// make all the tiles lower than the player higher than them on render layer 
-				spriteRenderer.sortingLayerName = "TreeLayer";
-			}
-			else
-			{
-				// give this tile a normal tile set render order
-				spriteRenderer.sortingLayerName = "Tileset";
-			}
+			// make all the tiles lower than the player higher than them on render layer 
+			spriteRenderer.sortingLayerName = "TreeLayer";
+		}
+		else
+		{
+			// give this tile a normal tile set render order
+			spriteRenderer.sortingLayerName = "Tileset";
 		}
 	}
 }
